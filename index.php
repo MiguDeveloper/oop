c47a6ebfe02bccba92ae17c22f7253a934a5b065
MiguDeveloper <miguel.peru.seo@gmail.com>
2018-06-19 01:00:39 -0500

    Encapsulamiento, getters and setters

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
 <?php
+
+class Person
+{
+    private $firsName;
+    private $lastName;
+
+    /**
+     * Person constructor.
+     * @param $firsName
+     * @param $lastName
+     */
+    public function __construct($firsName, $lastName)
+    {
+        $this->firsName = $firsName;
+        $this->lastName = $lastName;
+    }
+
+    public function getFirsName()
+    {
+        return $this->firsName;
+    }
+
+    public function setFirsName($firsName)
+    {
+        $this->firsName = $firsName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    function getNombreCompletos()
+    {
+        return $this->firsName . " " . $this->lastName;
+    }
+}
+
+// Instanciamos la clase
+$persona1 = new Person("Miguel Angel", "Chinchay");
+$persona1->setFirsName("Miguel");
+
+$persona2 = new Person("Steve", "Jobs");
+
+echo "{$persona1->getNombreCompletos()}, es amigo de {$persona2->getNombreCompletos()}";
+echo "<br>";
+echo "Solo nombre del primero: {$persona1->getFirsName()}";
